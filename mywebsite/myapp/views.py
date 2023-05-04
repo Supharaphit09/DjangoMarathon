84bdb1735f5168dc1e681058896d25bcf6d8ab86
Supharaphit09 <114590778+Supharaphit09@users.noreply.github.com>
2023-05-04 13:09:10 +0700

    Add files via upload

--- mywebsite/myapp/views.py
+++ mywebsite/myapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def Homepage(request):
-    return HttpResponse('<h1>Hello world')
+    return HttpResponse(request,'myapp/homepage.html')
